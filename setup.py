bed4b6b8af4b28557e50813c93893758793f0d84
Daniel Vorberg <dv@pks.mpg.de>
2017-04-22 19:18:13 +0200

    Rename package directory smart_manuscript to smartmanuscript

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(
-    name='smart_manuscript',
+    name='smartmanuscript',
     version='0.2',
     description='Transcribe (digitize) handwritten manuscripts and digitizer pen input',
     author='Daniel Vorberg',
     author_email='dv@pks.mpg.de',
     url='https://github.com/antemons/smart-manuscript',
-    packages=['smart_manuscript'],
-    package_dir={'smart_manuscript': 'smart_manuscript'},
+    packages=['smartmanuscript'],
+    package_dir={'smartmanuscript': 'smartmanuscript'},
     scripts=["transcribe", "manuscript-writer"],
     include_package_data = True,
     long_description=""" """)
