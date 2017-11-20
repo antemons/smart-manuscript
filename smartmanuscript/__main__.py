ddd34c449b240c294b6b351e020774000c50248b
Daniel Vorberg <dv@pks.mpg.de>
2017-11-20 17:35:28 +0100

    add __main__.py

---
+++ smartmanuscript/__main__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+    This file is part of Smart Manuscript.
+
+    Smart Manuscript (transcript handwritten notes or inputs)
+    Copyright (c) 2017 Daniel Vorberg
+
+    Smart Manuscript is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Smart Manuscript is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Smart Manuscript.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+__author__ = "Daniel Vorberg"
+__copyright__ = "Copyright (c) 2017, Daniel Vorberg"
+__license__ = "GPL"
+
+from .transcribe import main
+
+main()
+
