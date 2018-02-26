99bb58e81e87d9e7992d4fca45b68c6219c05e83
Daniel Vorberg <dv@pks.mpg.de>
2018-02-26 12:17:40 +0100

    Show handwriting by an interpolated spline (smoothing)

--- smartmanuscript/application.py
+++ smartmanuscript/application.py
@@ -23,15 +23,17 @@ __author__ = "Daniel Vorberg"
 __copyright__ = "Copyright (c) 2017, Daniel Vorberg"
 __license__ = "GPL"
 
-
 from tensorflow.python.platform.app import flags
 import numpy as np
+from scipy import interpolate
 import os
 import sys
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
+import cairo
+
 try:
     from xinput import operate_xinput_device, MODE_ENABLE, MODE_DISABLE
     XINPUT_IS_IMPORTED = True
@@ -54,6 +56,39 @@ def read_flags():
     return flags.FLAGS
 
 
+def stroke_to_spline(stroke):
+    if len(stroke) == 1:
+        t = 4 * [0] + 4 * [1]
+        c = np.array(stroke)[0][:, None] * np.ones(4)[None, :]
+        spline = (t, c, 3)
+    elif len(stroke) == 2:
+        t = 4 * [0] + 4 * [1]
+        c = np.stack([stroke[0],
+                      stroke[1],
+                      stroke[0],
+                      stroke[1]])
+        c = list(c.transpose())
+        spline = (t, c, 3)
+    elif len(stroke) == 3:
+        (t, c, k), _ = interpolate.splprep(np.array(stroke).transpose(), u=None, k=2, s=0)
+        c = np.array(c)
+        new_c = np.stack([c[:, 0],
+                          1/3 * c[:, 0] + 2/3 * c[:, 1],
+                          2/3 * c[:, 1] + 1/3 * c[:, 2],
+                          c[:, 2]])
+        new_c = list(new_c.transpose())
+        new_t = 4 * [0] + 4 * [1]
+        spline = (new_t, new_c, 3)
+    else:
+        spline, u = interpolate.splprep(
+            np.array(stroke).transpose(), k=3, s=len(stroke) * 10)
+        for knot in 3 * list(spline[0][4:-4]):
+            spline = interpolate.insert(knot, spline)
+    control_points = np.array(spline[1]).transpose()
+    control_points = control_points[:max(len(control_points)-4, 4)]
+    return control_points
+
+
 class HandwrittenInput(Gtk.Window):
     """ Window to record the handwriting and show its recognation
     """
@@ -71,6 +106,7 @@ class HandwrittenInput(Gtk.Window):
         self._deactivate_touchscreen = deactivate_touchscreen
         self.recognizer = Reader(model_path=model_path)
         self.strokes = []   # on-line writing information, grouped by strokes
+        self.splines = []
 
         self.set_title("Smart Manuscript Writer")
         self.resize(1000, 300)
@@ -106,22 +142,23 @@ class HandwrittenInput(Gtk.Window):
         if XINPUT_IS_IMPORTED and self._deactivate_touchscreen:
             operate_xinput_device(MODE_DISABLE, self._deactivate_touchscreen)
 
-    def draw_auxiliary_lines(self, cairo):
-        cairo.set_source_rgb(0.5, 0.5, 0.5)
-        cairo.set_line_width(.5)
+    def draw_auxiliary_lines(self, cr):
+        cr.set_source_rgb(.5, .5, .5)
+        cr.set_line_width(.5)
 
-        cairo.move_to(0, 100)
-        cairo.line_to(1000, 100)
-        cairo.stroke()
+        cr.move_to(0, 100)
+        cr.line_to(1000, 100)
 
-        cairo.move_to(0, 200)
-        cairo.line_to(1000, 200)
-        cairo.stroke()
+        cr.move_to(0, 200)
+        cr.line_to(1000, 200)
+
+        cr.stroke()
 
     def on_button_clicked(self, widget):
         """ clear board, hide suggestion buttons
         """
         self.strokes = []
+        self.splines = []
         self.canvas.queue_draw()
         text = self.clipboard.wait_for_text()
         if text is None:
@@ -142,6 +179,7 @@ class HandwrittenInput(Gtk.Window):
     def on_button_press(self, _, event):
         """ store starting point for new stroke, hide suggestion buttons
         """
+
         self.strokes.append([[event.x, event.y]])
         self.canvas.queue_draw()
         for button in self.buttons:
@@ -150,6 +188,7 @@ class HandwrittenInput(Gtk.Window):
     def on_button_release(self, *_):
         """ make transcription suggestions
         """
+        self.splines.append(stroke_to_spline(self.strokes[-1]))
         self.recognize()
 
     def on_mouse_move(self, _, event):
@@ -164,23 +203,28 @@ class HandwrittenInput(Gtk.Window):
     def on_draw(self, _, cr):
         """ redraw the ink
         """
+
         self.draw_auxiliary_lines(cr)
+
+        cr.set_line_cap(cairo.LINE_CAP_ROUND)
+        cr.set_line_join(cairo.LINE_JOIN_BEVEL)
 
         cr.set_source_rgb(0, 0, 0)
         cr.set_line_width(2)
 
-        for stroke in self.strokes:
-            for i, point in enumerate(stroke):
-                if len(stroke) == 1:
-                    radius = 2
-                    cr.arc(point[0], point[1],
-                           radius, 0, 2.0 * np.pi)
-                    cr.fill()
-                    cr.stroke()
-                elif i != 0:
-                    cr.move_to(stroke[i-1][0], stroke[i-1][1])
-                    cr.line_to(point[0], point[1])
-                    cr.stroke()
+        for spline in self.splines:
+
+            cr.move_to(*spline[0])
+            for control_1, control_2, knot in zip(spline[1:][::4],
+                                                  spline[2:][::4],
+                                                  spline[3:][::4]):
+                cr.curve_to(*control_1, *control_2, *knot)
+
+        for stroke in self.strokes[len(self.splines):]:
+            cr.move_to(*stroke[0])
+            for dot in stroke:
+                cr.line_to(*dot)
+        cr.stroke()
 
     def recognize(self):
         """ suggest transcrition and show it
